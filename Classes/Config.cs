02a5585b1f05032f196d59628d4ccc3d564dfc43
NatanFigueiredo <natanfigueiredo27@gmail.com>
2020-10-02 12:22:18 -0300

    add codigo fonte local

---
+++ Classes/Config.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demoApadrinhamento.Classes
+{
+    class Config
+    {
+        string csvAlunos = @"C:\Users\natan\Desktop\CPE\Apadrinhamento 2020-2\csvAlunos.csv";
+        string csvPadrinhos = @"C:\Users\natan\Desktop\CPE\Apadrinhamento 2020-2\csvPadrinhos.csv";
+        string saida = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+        public string CsvAlunos { get => csvAlunos; }
+        public string CsvPadrinhos { get => csvPadrinhos; }
+        public string Saida { get => saida; }
+    }
+}
---
+++ Classes/Match.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demoApadrinhamento.Classes
+{
+    class Match
+    {
+        public Match(Aluno afilhado, Padrinho padrinho)
+        {
+            this.afilhado = afilhado;
+            this.padrinho = padrinho;
+        }
+
+        public Aluno afilhado { get; set; }
+        public Padrinho padrinho { get; set; }
+
+    }
+}
